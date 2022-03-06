801754dad3f1f03e8f13c82e53b4b7345ad6f3cf
d <d>
2022-03-06 11:02:32 -0800

    init

---
+++ param.py
@@ -0,0 +1,3 @@
+ALL_CHARACTERS = ["Leonard", "Sheldon", "Howard", "Rajesh", "Penny", "Bernadette", "Amy", "Others"]
+
+QUESTIONS = ["What does person on the right look like?", "How many people?", "How does the person on the right feel?", "How is the environment feeling?"]
